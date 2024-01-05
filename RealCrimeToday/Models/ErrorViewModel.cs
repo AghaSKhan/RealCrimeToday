8708ba8c8dd2e4febd4f1406daefa1d4d6698b6a
agha.khan <agha.khan@surprisinggames.net>
2024-01-04 18:27:20 -0800

    Add project files.

---
+++ RealCrimeToday/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RealCrimeToday.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
